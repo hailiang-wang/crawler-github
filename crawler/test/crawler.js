112778058a92b2ac1a19e2eb6422d7a3adb0ed92
Hai Liang Wang <hailiang.hl.wang@gmail.com>
2017-08-15 18:31:36 +0800

    crawler http://outofmemory.cn/

--- crawler/test/crawler.js
+++ crawler/test/crawler.js
@@ -1,24 +1,44 @@
-var assert = require('assert'),
-    should = require('should'),
+// var assert = require('assert'),
+//     should = require('should'),
+const test = require('ava'),
     crawler = require('../services/crawler');
+const cheerio = require('cheerio');
 
-describe('Web Pages Crawler', function() {
-    this.timeout(5000);
-
-    it('should get paths of http://bbs.byr.cn/#!board/Friends', function(done) {
-        crawler.queue({
-                uri: 'http://bbs.byr.cn/board/Friends',
-                headers: {
-                    'X-Requested-With': 'XMLHttpRequest'
-                }
-            })
-            .then(function(response) {
-                should(response.result).ok;
-                should(response.$).ok;
-                done();
-            })
-            .fail(function(err) {
-                done(err);
-            });
+function sleep(){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve()
+        }, 1000)
     })
+}
+
+function printAllUserIds(response){
+    var names = response.$('body > div > div.github_users > ul > li > div.name');
+    for(let i = 0;  i < names.length; i++){
+        let s = cheerio.load(names[i.toString()]);
+        let d = s.text().split(" ");
+        let github_id = d[d.length - 1];
+        console.log("github_id:", github_id)
+    }
+}
+
+test('Web Pages Crawler', async (t)=> {
+    // should get paths of http://bbs.byr.cn/#!board/Friends
+    let maxIndex = 1648
+    for(let index = 1; index <= maxIndex; index++){
+        console.log("index ", index)
+        await sleep();
+        let response = await crawler.queue({
+            uri: `http://outofmemory.cn/github/*/*/?sort=stars&page=${index}`,
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest'
+            }
+        });
+        printAllUserIds(response);
+    }
+    // should(response.result).ok;
+    // should(response.$).ok;
+
+
+    t.pass();
 })
